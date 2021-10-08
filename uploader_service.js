c9fe61f9b9727bd548bb4629b6a58bd39389791e
Etornam <sunumacbright@gmail.com>
2021-10-08 21:43:21 +0000

    Update uploader_service.js

--- uploader_service.js
+++ uploader_service.js
@@ -1,13 +1,12 @@
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const env = process.env.NODE_ENV || 'aws_credentials';
-const config = require('./config.json')[env];
+
 
 aws.config.update({
-  secretAccessKey: config.secretAccessKey,
-  accessKeyId: config.accessKeyId,
-  region: config.region
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  region: process.env.AWS_REGION
 });
 
 const s3 = new aws.S3();
@@ -16,7 +15,7 @@ const upload = multer({
   storage: multerS3({
     acl: 'public-read',
     s3,
-      bucket: config.bucket,
+      bucket: process.env.AWS_BUCKET,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
       cb(null, Object.assign({}, req.body));
