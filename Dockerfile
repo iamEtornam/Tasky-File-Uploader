69d572e60f38a7f1dfbfdd7913fb7ff1d8a7530c
Etornam Sunu <sunumacbright@gmail.com>
2023-03-08 21:23:04 +0000

    fix

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:14.15.5
-WORKDIR /app
-COPY package.json /app
-RUN npm install
-EXPOSE 4000
-COPY . /app
-CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -5,43 +5,31 @@ require('dotenv').config();
 const imageUploader = require('./image_controller');
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
-const env = process.env.NODE_ENV || 'development';
 
 const app = express();
 app.use(cors())
-app.use(logger('dev'));
 app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb'}));
+app.use(express.urlencoded({ limit: '100mb' }));
 
 Sentry.init({
-  environment: env,
-    dsn: process.env.SENTRY_KEY,
-    integrations: [
-      // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
-      new Tracing.Integrations.Express({ 
-        // to trace all requests to the default router
-        app, 
-        // alternatively, you can specify the routes you want to trace:
-        // router: someRouter, 
-      }),
-    ],
-  
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-  });
+  dsn: process.env.SENTRY_API_KEY,
+  environment: process.env.NODE_ENV,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 
-  // RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(logger("dev"));
 app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
-
-// the rest of your app
-
 app.use(Sentry.Handlers.errorHandler());
   
 const port = process.env.PORT || 4000;
